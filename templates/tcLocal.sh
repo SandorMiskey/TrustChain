bfc813f62c445da7e16229ffe6462b365fbe576e
Sandor Miskey <s@ndor.miskey.net>
2023-08-01 15:10:14 +0000

    ne need to update tcConf.sh alwayas...

--- templates/tcLocal.sh
+++ templates/tcLocal.sh
@@ -10,6 +10,9 @@ export TC_PATH_BASE=/home/smiskey/TrustChain
 export TC_PATH_RC=${TC_PATH_BASE}/scripts/tcConf.sh
 export COMMON_FUNCS=${TC_PATH_BASE}/scripts/commonFuncs.sh
 
+export TC_EXEC_FORCE=true
+export TC_EXEC_SURE=true
+
 export TC_RAWAPI_PEERENDPOINT=localhost:8101
 
 # endregion: init
