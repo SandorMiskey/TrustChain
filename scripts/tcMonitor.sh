7db995f1c43b4c610f42dcc8e8c10a4247266bc6
Sandor Miskey <s@ndor.miskey.net>
2023-08-23 15:19:09 +0000

    uptimes

--- scripts/tcMonitor.sh
+++ scripts/tcMonitor.sh
@@ -21,11 +21,18 @@ commonPP $TC_PATH_SCRIPTS
 # endregion: common
 
 
-function _uptime() {
+function _load() {
 	local -n peer=$1
 	local cmd='uptime | awk -F "load average:" "{print $2}"'
 	local uptime=$( ssh ${peer[node]} uptime )
 	local out=$( echo "$uptime" | /usr/bin/awk -F 'load average:' '{print "\t" $2}' )
+	commonPrintf "${peer[node]} -> $out"
+
+}
+
+function _uptime() {
+	local -n peer=$1
+	local out=$( ssh ${peer[node]} "uptime --pretty" )
 	commonPrintf "${peer[node]} -> $out"
 
 }
@@ -50,6 +57,11 @@ commonPrintf " "
 commonPrintf "services"
 commonPrintf "
 $( docker service ls --format 'table{{.Name}}\t{{.Mode}}\t{{.Replicas}}' )"
+
+commonPrintf " "
+commonPrintf "loads"
+commonPrintf " "
+commonIterate _load "ignore|checking |array|node|:" "${TC_SWARM_MANAGERS[@]}" "${TC_SWARM_WORKERS[@]}"
 
 commonPrintf " "
 commonPrintf "uptimes"
